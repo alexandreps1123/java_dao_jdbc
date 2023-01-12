1be251af299d2ad03ec0599de05e6cbca2199ae0
alexandreps1123 <alexandre.ps1123@gmail.com>
2023-01-11 21:01:40 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DAOFactory;
@@ -31,5 +32,10 @@ public class Program {
             System.out.println(obj);
         }
 
+        System.out.println("\n------------TESTE 4-------------");
+        Seller newSeller = new Seller(null, "EU", "eu@email.com", new Date(), 4000.0, dep);
+
+        sellerDAO.insert(newSeller);
+        System.out.println("Inserted! New id: " + newSeller.getId());
     }
 }
